1f725f80a0f715aa1196e99d548183b5b9d0f4f4
mrcodefrost <mr.codefrost@gmail.com>
2024-10-08 18:37:51 +0530

    order model & controller added

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import userRouter from "./routes/userRoute.js";
 
 import 'dotenv/config';
 import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
 
 
 // import bodyParser from "body-parser";
@@ -34,6 +35,7 @@ app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
 app.use("/api/user", userRouter);
 app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
 
 app.get("/", (req, res) => {
     res.send("Server is ready");
